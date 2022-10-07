9687e1c389e4bab0d18d01d0ed10436fb0fec6ab
omerbastug <bomersami@gmail.com>
2022-10-07 17:34:50 +0200

    testing

--- api/apiapp.js
+++ api/apiapp.js
@@ -12,3 +12,7 @@ app.use((req,res,next)=>{
 app.use("/reservation",resrouter);
 app.use("/user/",userrouter);
 app.use("/post", postRouter)
+
+app.get("/",(req,res)=>{
+    res.send("Testing..")
+})
